ad0d44d7b9c72c08298f89618ead8f34e23fff28
RexT <baptiste.marmierskyrm@hotmail.fr>
2019-04-18 09:31:52 +0200

    Ajout des modules

---
+++ encode_b64_zip.js
@@ -0,0 +1,21 @@
+/*********** DEVELOPPER PAR REXT ************/
+/*                                          */
+/*      Pourquoi ce programme ?             */
+/*      Parce que ça peut servir XD         */
+/*                                          */
+/********************************************/
+
+
+
+
+/*********** MANUEL D'UTILISATION ***********/
+/*                                          */
+/*  Pour lancer  l'installation  :  npm i   */
+/*                                          */
+/********************************************/
+
+
+/****LES MODULES NODEJS****/
+const fs = require('fs');
+const btoa =  require('btoa');
+const AdmZip = require('adm-zip');
