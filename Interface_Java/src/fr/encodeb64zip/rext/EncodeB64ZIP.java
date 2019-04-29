53c90f55d9d2b95ff4e37590eb03bbcab6977417
RexT <baptiste.marmierskyrm@hotmail.fr>
2019-04-29 17:12:28 +0200

    Création de la JFrame

--- Interface_Java/src/fr/encodeb64zip/rext/EncodeB64ZIP.java
+++ Interface_Java/src/fr/encodeb64zip/rext/EncodeB64ZIP.java
@@ -1,5 +1,7 @@
 package fr.encodeb64zip.rext;
 
+import java.awt.Font;
+
 import javax.swing.*;
 
 public class EncodeB64ZIP {
@@ -8,11 +10,13 @@ public class EncodeB64ZIP {
 	{
 		// TODO Auto-generated method stub
 		
+		// On déclare notre JFrame et on nomme notre fenêtre
 		JFrame frame = new JFrame("Encode base64 & zip");
 		
-		frame.setIconImage(new ImageIcon("img/icon.png").getImage());
+		// On attribut une icone 
+		frame.setIconImage(new ImageIcon("img/icon.jpg").getImage());
 		
-		frame.setSize(600, 400);
+		frame.setSize(800, 500);
 		
 		frame.setLocationRelativeTo(null);
 		
@@ -28,8 +32,16 @@ public class EncodeB64ZIP {
 		
 	}
 
-	private static void placeComponents(JPanel panel) {
+	private static void placeComponents(JPanel panel) 
+	{
 		// TODO Auto-generated method stub
+		
+		panel.setLayout(null);
+		
+		JLabel titreLabel = new JLabel("Options de l'application");
+		titreLabel.setBounds(275, 30, 230, 35);
+		titreLabel.setFont(new java.awt.Font(Font.SANS_SERIF, Font.CENTER_BASELINE, 20));
+		panel.add(titreLabel);
 		
 	}
 
